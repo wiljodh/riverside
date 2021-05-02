76411d4765b61e1cfc7c7332de7528a834e6309b
wiljodh <wiljodh12@gmail.com>
2021-05-02 21:47:52 +0700

    Initial Commit

---
+++ app/Http/Controllers/PageController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\user;
+
+class PageController extends Controller
+{
+    public function home()
+    {
+        return view('index');
+    }   
+
+    public function about()
+    {
+        return view('about');
+    }
+
+    public function faqs()
+    {
+        return view('faqs');
+    }
+
+    public function contactus()
+    {
+        return view('contact');
+    }
+
+    public function gallery()
+    {
+        return view('gallery');
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+// PAGE ROUTE
+Route::get('/', 'App\Http\Controllers\PageController@home');
+Route::get('/about', 'App\Http\Controllers\PageController@about');
+Route::get('/faqs', 'App\Http\Controllers\PageController@faqs');
+Route::get('/gallery', 'App\Http\Controllers\PageController@gallery');
+// Route::get('/contact', 'App\Http\Controllers\PageController@contact');
+
+// MAILER
+Route::get('/contact', 'App\Http\Controllers\EmailusController@showContactForm');
+Route::post('/contact', 'App\Http\Controllers\EmailusController@sendMail');
